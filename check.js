e6c215c61c0d8bdecbe0fd95bd361be3b4a7f8e2
xpert-uv <72463141+xpert-uv@users.noreply.github.com>
2021-03-28 23:59:43 -0400

    done

---
+++ check.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+console.log(process.argv[4]);
--- step3.js
+++ step3.js
@@ -3,6 +3,41 @@ const fs = require('fs');
 const axios = require('axios');
 
 
+
+function writeFile(content, filename) {
+    fs.writeFile(filename, content, 'utf8', err => {
+        if (err) {
+            console.log("Erorrrr......" + err);
+        } else {
+            console.log(content);
+
+        }
+    })
+
+
+};
+
+function cats(path, output) {
+    fs.read(path, 'utf8', err => {
+        if (err) {
+            console.log("Errorr......." + err);
+            process.exit(1);
+        } else {
+            writeFile(data, output);
+        }
+    })
+}
+
+async function webCats(url, output) {
+    let response = await axios.get(url);
+    console.log(response);
+    if (response.status === 200) {
+        writeFile(response.data, output);
+    } else { }
+    console.log("Error....")
+}
+
+
 function cat(path) {
     fs.readFile(path, 'utf8', function (err, data) {
         if (err) {
@@ -15,9 +50,6 @@ function cat(path) {
 
 }
 
-
-
-
 async function webCat(url) {
     let resp = await axios.get(`${url}`);
     if (resp.status == 200) {
@@ -29,9 +61,26 @@ async function webCat(url) {
 }
 
 let path = process.argv[2];
+console.log(path);
 
-if (path.slice(0, 4) === 'http') {
-    webCat(path);
+if (path === '--out') {
+    path = process.argv[4];
+    console.log(path);
+    let filename = process.argv[3];
+    console.log(filename);
+    if (path.slice(0, 4) === 'http') {
+        webCats(path, filename);
+    } else {
+        cats(path, filename);
+    }
+
+
 } else {
-    cat(path);
+    if (path.slice(0, 4) === 'http') {
+        webCat(path);
+    } else {
+        cat(path);
+    }
+
 }
+
